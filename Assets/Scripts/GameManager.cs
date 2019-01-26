4a6ec3776bdba59e73f1453fe673b06c72f5e0ad
Matt <mattjenkinswork@gmail.com>
2019-01-26 15:05:05 +0000

    Initial Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    string input;
+    public enum AttackStyle {above,fastSide,arcing,noDamage}//Wiggly handled elsewhere
+    public AttackStyle style;
+
+    public GameObject[] letterObjects;
+
+    char[] letters;
+
+    public float letterSpacing = 1;
+    public float wordSpacing = 1;
+
+
+    void Awake()
+    {
+        letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.!?'".ToLower().ToCharArray();
+        WriteOut(AttackStyle.above, 0);
+
+        
+    }
+
+    //Wave management stuff here
+
+
+    void WriteOut(AttackStyle attack, int stringID )
+    {
+        string text = FindText(stringID);
+
+        text = text.ToLower();
+
+        char[] chars = text.ToLower().ToCharArray();
+
+        //FIND SPECIAL CHARACTERS HERE TOO
+
+        text = "";
+
+        List<int> letterIDs = new List<int>();
+
+        bool inWord = false;
+
+        GameObject currentParent = null;
+
+        Vector3 nextLetterPosition = Vector3.zero;
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+
+            //Finding letter id's to use for instantiation
+            for (int m = 0; m < letters.Length; m++)
+            {
+                if (chars[i] == letters[m])
+                {
+                    letterIDs.Add(m);
+                    break;
+                }
+            }
+
+            //Create letters
+
+            if (inWord && letterIDs[i] == 26)
+            {
+                inWord = false;
+            }
+
+            if (!inWord)
+            {
+                inWord = true;
+                currentParent = new GameObject();
+                currentParent.transform.position = nextLetterPosition;
+            }
+
+            if (letterIDs[i] != 26)
+            {
+                 GameObject currentLetter = Instantiate(letterObjects[letterIDs[i]], currentParent.transform,false);
+                nextLetterPosition.x += letterSpacing;
+            }
+
+            nextLetterPosition.x += wordSpacing;
+
+        }
+
+        
+
+    }
+
+    string FindText(int id)
+    {
+        switch (id)
+        {
+            case 0:
+                return "fuck my life";
+            case 1:
+                return "poop";
+            case 2:
+                return "poop";
+            case 3:
+                return "poop";
+
+
+
+
+
+        }
+
+        return "Failure While Finding String";
+    }
+
+
+}
---
+++ Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObject : MonoBehaviour {
+
+    public GameObject[] itemToPlace;
+    public GameObject[] holograms;
+
+    Camera cam;
+
+    Ray ray;
+
+    RaycastHit hit;
+
+
+    public Vector3 placementOffset;
+
+    public int itemIndex = 0;
+
+    GameObject currentHolo;
+
+    public Vector3 holoPos;
+
+    Material mat;
+
+    public bool canPlace = false;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(currentHolo != null)
+            mat = currentHolo.GetComponent<Renderer>().material;
+
+
+        ItemHover();
+
+        if (Input.GetMouseButtonDown(0))
+            PlaceItem();
+	}
+
+    void ItemHover()
+    {
+
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.CompareTag("Overlay"))
+        {
+            holoPos = new Vector3(hit.point.x + placementOffset.x, hit.point.y + placementOffset.y, hit.point.z + placementOffset.z);
+
+        }
+        else if (currentHolo != null)
+            Destroy(currentHolo);
+
+       
+
+        if (Physics.Raycast(ray, out hit) && currentHolo == null)
+        {
+            if (hit.collider.gameObject.CompareTag("Overlay"))//Probably more conditions here later
+            {
+                currentHolo = Instantiate(holograms[itemIndex], holoPos, Quaternion.identity);
+            }
+        }
+
+        if(currentHolo != null)
+        {
+            currentHolo.transform.position = holoPos;
+
+            Collider[] foundCols = Physics.OverlapBox(holoPos, currentHolo.GetComponent<Renderer>().bounds.extents);
+
+            if(mat != null)
+            {
+                mat.color = Color.green;
+                //placementOffset.z += 0.1f;
+            }
+                
+
+            canPlace = true;
+
+
+            for (int i = 0; i < foundCols.Length; i++)
+            {
+                if (foundCols[i].gameObject.CompareTag("Prop") || foundCols[i].gameObject.CompareTag("Floor"))
+                {
+                    mat.color = Color.red;
+                    //placementOffset.z -= 0.1f;
+                    canPlace = false;
+                }
+            }
+
+            
+
+
+        }
+
+    }
+
+    void PlaceItem()
+    { 
+
+        if (currentHolo != null && canPlace)
+        {
+            Instantiate(itemToPlace[itemIndex], holoPos, Quaternion.identity);
+        }
+    }
+
+
+
+}
